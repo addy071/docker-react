17707dd67a0b8ed3497cf5172a9c2ce59c69f66e
adnan <adnan.spahic@kulturit.no>
2018-12-18 15:55:23 +0100

    FIX:changed text in App.js!

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p className="App-intro">
+            Feature breanch changed!
+          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
